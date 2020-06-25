de0e16ac0b7dc444f3fa6e7f9451efbbf8f44d79
omipatel <ohm.patel1997@gmail.com>
2020-06-25 17:48:23 +0530

    line chunks reduced

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"strings"
 	"sync"
-	"sync/atomic"
+
 	"time"
 )
 
@@ -27,19 +27,16 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	linesChunkLen := 100000 * 1024
-	linesChunkPoolAllocated := int64(0)
+	linesChunkLen := 10000 * 1024
+
 	linesPool := sync.Pool{New: func() interface{} {
 		lines := make([]string, 0, linesChunkLen)
-		atomic.AddInt64(&linesChunkPoolAllocated, 1)
 		return lines
 	}}
 	lines := linesPool.Get().([]string)[:0]
 
-	logsPoolAllocated := int64(0)
 	logsPool := sync.Pool{New: func() interface{} {
 		entries := make([]Log, 0, linesChunkLen)
-		atomic.AddInt64(&logsPoolAllocated, 1)
 		return entries
 	}}
 
@@ -52,25 +49,27 @@ func main() {
 			linesToProcess := lines
 			wg.Add(1) // add the count once every 100000 lines
 			go func() {
-
+				//decrease the count
+				defer wg.Done()
 				entries := logsPool.Get().([]Log)[:0]
+				defer linesPool.Put(linesToProcess) // put back the line slice in pool
+				//ParseAndIndexBulk()
+				defer logsPool.Put(entries) //put back the log slice in pool
+
 				for _, text := range linesToProcess {
 
 					entry := Log{}
-					timeStampEndingIndex := strings.Index(text, ",")
-					entry.Message = text[:timeStampEndingIndex]
-					logCreationTime := text[timeStampEndingIndex+1:]
+					logSlice := strings.SplitN(text, ",", 2)
+					logCreationTime := logSlice[0]
+					entry.Message = logSlice[1]
 
-					if entry.CreatedOn, err = time.Parse("2006-01-02T15:04:05-0700Z", logCreationTime); err != nil {
-						fmt.Printf("Could not able to parse the time :%s for log : %v", logCreationTime, entry)
+					if entry.CreatedOn, err = time.Parse("2006-01-02T15:04:05.0000Z", logCreationTime); err != nil {
+						fmt.Printf("Could not able to parse the time :%s for log : %v", logCreationTime, text)
 						return
 					}
 					entries = append(entries, entry)
 				}
-				linesPool.Put(linesToProcess) // put back the line slice in pool
-				//ParseAndIndexBulk()
-				logsPool.Put(entries) //put back the log slice in pool
-				wg.Done()             //decrease the count
+
 			}()
 			lines = linesPool.Get().([]string)[:0] // get the new lines pool to store the new lines
 		}
@@ -79,8 +78,8 @@ func main() {
 		}
 	}
 	wg.Wait()
-	fmt.Printf("Name time: %v\n", time.Since(start))
+	fmt.Printf("\n time: %v\n", time.Since(start))
 
 }
 
-//func ParseAndIndexBulk()
+func ParseAndIndexBulk()
